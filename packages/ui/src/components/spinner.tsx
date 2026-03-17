72cb2dde1484335119f9ab0cb9dd1bf4bdd3f902
Nicolas Becharat <nicolas.becharat@gmail.com>
2026-03-17 15:47:06 +0100

    feat: typecheck

--- packages/ui/src/components/spinner.tsx
+++ packages/ui/src/components/spinner.tsx
@@ -1,7 +1,8 @@
+import type { RemixiconComponentType } from "@remixicon/react"
 import { RiLoaderLine } from "@remixicon/react"
 import { cn } from "@workspace/ui/lib/utils"
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+function Spinner({ className, ...props }: Omit<React.ComponentProps<RemixiconComponentType>, "children">) {
   return (
     <RiLoaderLine role="status" aria-label="Loading" className={cn("size-4 animate-spin", className)} {...props} />
   )
